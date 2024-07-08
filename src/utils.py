efd89694792ada055431320d4e4d7b9e4a157b18
LmG Syphxn <f20230254@dubai.bits-pilani.ac.in>
2024-07-08 23:47:19 +0400

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -31,3 +31,10 @@ def evaluate_models(xtrain,ytrain,xtest,ytest,models):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
