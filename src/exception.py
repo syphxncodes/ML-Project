da667fb27b8b4936ea9268adfab58427293675fa
LmG Syphxn <f20230254@dubai.bits-pilani.ac.in>
2024-07-08 12:15:37 +0400

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 import sys
 import logging
 import logger
-from src.logger import logging
+from logger import logging
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
