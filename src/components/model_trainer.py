09648d1d66ddf6f8c4502331b39146d65b2ce5bc
syphxncodes <143844745+syphxncodes@users.noreply.github.com>
2024-07-15 23:56:32 +0400

    Update model_trainer.py

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -56,7 +56,7 @@ class ModelTrainer:
             ]
             best_model=models[best_model_name]
             if best_model_score<0.6:
-                raise CustomException("No best model found :(")
+                raise CustomException("No best model found :(",sys)
             logging.info(f"Best found model on both training and testing dataset")
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
